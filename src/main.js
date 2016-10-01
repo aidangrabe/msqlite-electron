0dfb7e36c149acb912c1120a43b6cce2b036616b
Aidan Grabe <aidan@teamwork.com>
2016-10-01 15:48:33 +0100

    Add tab for quickly viewing tables

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 // You can also require other files to run in this process
 const Prefs = require('../../src/Prefs.js')
-const exec = require('child_process').exec;
+const exec = require('child_process').exec
 const prefsFile = 'prefs.json'
 
 const sqlQueryField = document.getElementById('sql-query')
 const sqlOutput = document.getElementById('sql-output')
 const packageNameField = document.getElementById('package-name-field')
 const databaseNameField = document.getElementById('database-name-field')
+
+const tablesDiv = document.getElementById('tables')
+const tablesData = document.getElementById('table-data')
+
+const homeTab = document.getElementById('home-tab')
+const queryTab = document.getElementById('query-tab')
+
+const nav = document.getElementById('nav')
+const navLinks = nav.querySelectorAll("a").forEach(function(it) {
+	const href = it.getAttribute("href")
+	it.addEventListener('click', function(event) {
+		event.preventDefault()
+		switchTab(href)
+	})
+})
 
 const prefs = new Prefs.Preferences(prefsFile)
 const history = []
@@ -15,13 +30,48 @@ let currentHistoryIndex = 0
 prefs.load(function() {
 	packageNameField.value = prefs.getProperty("package") || ""
 	databaseNameField.value = prefs.getProperty("database") || ""
+
+	switchTab("#tables")
 })
+
+function switchTab(tab) {
+	homeTab.style.display = 'none'
+	queryTab.style.display = 'none'
+
+	const packageName = prefs.getProperty("package")
+	const databaseName = prefs.getProperty("database")
+
+	if (tab === "#tables") {
+		homeTab.style.display = 'block'
+		const cmd = `adb exec-out "run-as ${packageName} sqlite3 -html -header databases/${databaseName} '.tables'"`
+		runSqliteCommand('.tables', function(error, stdout, stderr) {
+			const output = stdout
+			const tableNames = output.split(" ")
+				.map(function(it) {
+					return it.trim()
+				})
+				.filter(function(it) {
+					return it.length > 0
+				})
+				.map(function(it) {
+					return `<li><a href="#" onclick="javascript:showTable('${it}')" class="table-button">${it}</a></li>`
+				})
+				.reduce(function(left, right) {
+					return left + " " + right
+				})
+			tablesDiv.innerHTML = "<div class='info'>Tables: <ul>" + tableNames + "</ul></div>"
+		})
+	}
+
+	if (tab === "#query") {
+		queryTab.style.display = 'block'
+	}
+}
 
 submitQuery = function() {
 	const packageName  = packageNameField.value
 	const databaseName = databaseNameField.value
 	const sqlQuery = sqlQueryField.value.replace(/\"|\'/g, '\\"')
-	const cmd = `adb exec-out "run-as ${packageName} sqlite3 -html -header databases/${databaseName} '${sqlQuery}'"`
 	
 	currentHistoryIndex = history.length
 	
@@ -31,13 +81,15 @@ submitQuery = function() {
 	
 	history.push(sqlQuery)
 	
-	exec(cmd, function(error, stdout, stderr) {
+	runSqliteCommand(sqlQuery, function(error, stdout, stderr) {
 		if (stdout.length == 0) {
 			sqlOutput.innerHTML = "<div class='info'>No Rows</div>"
 		} else if (stdout.startsWith('Error: ')) {
 			sqlOutput.innerHTML = "<div class='error'>" + stdout + "</div>"
-		} else {
+		} else if (stdout.indexOf("<TD>") != -1) {
 			sqlOutput.innerHTML = "<table>" + stdout + "</table>"
+		} else {
+			sqlOutput.innerHTML = "<pre>" + stdout + "</pre>"
 		}
 	})
 }
@@ -56,3 +108,18 @@ sqlQueryField.addEventListener('keydown', function(e) {
 		sqlQueryField.value = history[Math.abs(currentHistoryIndex) % history.length]
 	}
 })
+
+function showTable(tableName) {
+	const packageName = prefs.getProperty("package")
+	const databaseName = prefs.getProperty("database")
+	sqlQueryField.value = "SELECT * FROM " + tableName
+	switchTab("#query")
+	submitQuery()
+}
+
+function runSqliteCommand(command, callback) {
+	const packageName = prefs.getProperty("package")
+	const databaseName = prefs.getProperty("database")
+	const cmd = `adb exec-out "run-as ${packageName} sqlite3 -html -header databases/${databaseName} '${command}'"`
+	exec(cmd, callback)
+}
